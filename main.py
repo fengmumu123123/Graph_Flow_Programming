e52394b0a461837ee5cd1c77c7e36e994e0f8d79
fengmumu123123 <87262609+fengmumu123123@users.noreply.github.com>
2023-10-02 00:37:10 +0800

    模块化框架搭建
    Signed-off-by: fengmumu123123 <87262609+fengmumu123123@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QApplication, QMainWindow, QDockWidget, QTextEdit, QToolBar, QVBoxLayout, QWidget, \
     QMenuBar, QMenu, QStatusBar
